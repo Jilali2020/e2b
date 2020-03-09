6039c4349d675df8db86cb6fc0accdc37f737b04
Jilali2020 <61964106+Jilali2020@users.noreply.github.com>
2020-03-10 00:37:18 +0100

    Add files via upload

---
+++ e2bInstall.sh
@@ -0,0 +1,20 @@
+clear
+
+echo ""
+echo "> Even geduld  ... lichte update"
+opkg update  &>/dev/null
+
+echo ""
+echo "> Installatie Bash... in plaats van Shell"
+if opkg list_installed bash* | grep "bash*" &>/dev/null; then
+	echo "> Bash al geinstalleerd!"
+else
+	opkg install bash &>/dev/null
+fi
+
+wget -q https://raw.githubusercontent.com/Jilali2020/e2b/master/e2b.sh -O /tmp/install.sh
+chmod 777 /tmp/install.sh
+/tmp/install.sh
+
+rm ./install.sh
+exit
